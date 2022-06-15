f3f06997b9596f36593db5088b3d47b361c6f73e
gncEray <62249421+gncEray@users.noreply.github.com>
2022-06-15 21:13:35 +0300

    Add files via upload

---
+++ player.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    startplayer();
+}, false);
+
+var player;
+
+function startplayer() {
+    player = document.getElementById('video_player');
+    player.controls = false;
+}
+
+function play_vid() {
+    player.play();
+}
+
+function pause_vid() {
+    player.pause();
+}
+
+function change_vol() {
+    player.volume = document.getElementById("change_vol").value;
+}
